cd2ecc1e76588b8048564b8c1c655a1320301614
Angel-HC554 <angelhoil554@gmail.com>
2025-04-23 18:34:17 -0600

    Laravel Setup and Configuration

---
+++ app/Actions/Fortify/CreateNewUser.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Actions\Fortify;
+
+use App\Models\Doctor;
+use App\Models\User;
+use App\Models\UserDetails;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
+use Laravel\Jetstream\Jetstream;
+use Symfony\Component\Console\Input\Input;
+
+class CreateNewUser implements CreatesNewUsers
+{
+    use PasswordValidationRules;
+
+    /**
+     * Validate and create a newly registered user.
+     *
+     * @param  array<string, string>  $input
+     */
+    public function create(array $input): User
+    {
+        Validator::make($input, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => $this->passwordRules(),
+            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
+        ])->validate();
+
+        $user = User::create([
+            'name' => $input['name'],
+            'email' => $input['email'],
+            'type' => 'doctor', //only doctor can login and sign up 
+            'password' => Hash::make($input['password']),
+        ]);
+
+        $doctorInfo = Doctor::create(
+        [
+            'doc_id' => $user->id,
+            'status' => 'active',
+        ]);
+
+        return $user;
+    }
+}
